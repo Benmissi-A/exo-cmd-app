b57c5e8aa0216aac303f318e3e60932854e813de
Benmissi-A <aminebenmissi@hotmail.fr>
2021-03-18 17:48:17 +0100

    push

--- guessNumber.js
+++ guessNumber.js
@@ -1,9 +1,20 @@
 const readlinesync = require('readline-sync')
 const chalk = require('chalk');
 
-let number = readlinesync.question(`pour jouer avec moi entres le nombre au quel je pense !`)
-if(isNaN(Number(number))=== false){
-  if(Number(number) > Number(process.argv[2])){
-    console.log(`le nombre est trop grand`)
+let number =''
+do{
+
+ number = readlinesync.question(`entrez un nombre :`)
+  if(isNaN(Number(number))=== false){
+
+    if(Number(number) > Number(process.argv[2])){
+      console.log(chalk.red(`le nombre est trop grand`))
+    }else if(Number(number) < Number(process.argv[2])){
+       console.log(chalk.red(`le nombre est trop petit`))
+    }else{
+       console.log(chalk.green(`bravo! vous avez deviné`))
+    }
+  }else{
+    console.log(`err`)
   }
-}
+}while(Number(number) !== Number(process.argv[2]))
