69bd2733686e6d8d14e3acfc0729d830ca2c336b
Benmissi-A <aminebenmissi@hotmail.fr>
2021-03-18 16:45:36 +0100

    push

--- interMajorityTest.js
+++ interMajorityTest.js
@@ -1,10 +1,11 @@
 const readlinesync = require('readline-sync')
 
+
 let yourName = readlinesync.question(`entrez votre nom :`)
 let yourFirstName =  readlinesync.question(`entrez votre prenom :`)
 let yourAge =  readlinesync.question(`entrez votre prenom :`)
 
-if(Number(yourAge) < 18 ){
+if(Number(yourAge) < Number(process.argv[2]) ){
   console.log(`Désolé, ${yourFirstName} ${yourName}, vous êtes mineur, vous ne pouvez pas voter`)
 }else{
   console.log(`${yourFirstName} ${yourName}, vous êtes majeur, vous pouvez voter`)
--- pyramid.js
+++ pyramid.js
@@ -1,7 +1,17 @@
-const showStars = (nbStars,symbol) => {
-  for (let i = 1; i <= Number(nbStars); i += 1) {
-    console.log(symbol.repeat(i))
+const showStars = (nbStars,symbol, reverse ) => {
+  for (let i = 0; i < Number(nbStars); i += 1) {
+    switch(reverse){
+      case undefined :
+        console.log(symbol.repeat(i+1))
+        break
+      case '-r':
+        console.log(symbol.repeat(Number(nbStars)-i))
+        break
+      default :
+      console.log('error '.repeat(Number(nbStars)-i))
+      console.log('error '.repeat(i+1))
+      break
+    }
   }
 }
-
-pyramid(process.ard[2],process.ard[3])
+showStars(process.argv[2],process.argv[3],process.argv[4])
