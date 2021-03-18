0e469b0d7adb53bcd58119b7a03d582a43474847
Benmissi-A <aminebenmissi@hotmail.fr>
2021-03-18 15:26:42 +0100

    push

---
+++ pyramid.js
@@ -0,0 +1,7 @@
+const showStars = (nbStars,symbol) => {
+  for (let i = 1; i <= Number(nbStars); i += 1) {
+    console.log(symbol.repeat(i))
+  }
+}
+
+pyramid(process.ard[2],process.ard[3])
---
+++ sayMyName.js
@@ -0,0 +1,1 @@
+console.log(`My name is  ${process.argv[2]}`) 
